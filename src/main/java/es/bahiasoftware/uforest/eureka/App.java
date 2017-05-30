f61a8378263f8c351ca7dfddf8b6f2a587e52e57
Francisco Jose Ferreiro Viñas <ffervin@bahia.net>
2017-05-30 10:12:50 +0200

    first commit

---
+++ src/main/java/es/bahiasoftware/uforest/eureka/App.java
@@ -0,0 +1,16 @@
+package es.bahiasoftware.uforest.eureka;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+	
+	
+	public static void main(String[] args) throws Exception {
+        
+	}
+    
+}
